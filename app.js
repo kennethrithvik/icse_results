8b6c96e2fb923f23002b92c25c8ef88720376539
Kenneth Rithvik <kennethrithvik@yahoo.in>
2017-06-05 12:31:52 +0530

    used request instead of http-request

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-var http = require("http-request");
+var http = require("request");
 var cheerio = require("cheerio");
 var fs=require('fs');
 
 
-var url1 = 'cisce2017rprod090.azurewebsites.net/Results/Result/ShowResult?courseCode=ICSE&uniqueId=';
+var url1 = 'http://cisce2017rprod090.azurewebsites.net/Results/Result/ShowResult?courseCode=ICSE&uniqueId=';
 var uniqueId = 0; //6599699;
 var url2 = '&captcha=QKBPU&code=at0CccvjUbWHWKB33MF3qQ%3D%3D';
 
-for (let i = 6599900; i < 6599999; i++) {
-  http.get(url1 + i + url2,
-    function(err, res) {
-      result = res.buffer.toString();
+for (let i = 6577000; i < 6577999; i++) {
+  http(url1 + i + url2,
+    function(err, res, body) {
+      result = body.toString();
       const $ = cheerio.load(result);
       let parsedData = $("#resultpanal tr").text().replace(/\s+/g, ' ');
       if (parsedData.length > 5) {
